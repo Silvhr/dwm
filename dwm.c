caf4fc76744566a11a273c60d6961ef6c29422db
Silvhr <silvhr@protonmail.com>
2022-07-13 23:48:59 -0500

    increased refresh rate when moving windows

--- dwm.c
+++ dwm.c
@@ -1305,7 +1305,8 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			/* changed 1000/ 60 to 1000/1000 */
+			if ((ev.xmotion.time - lasttime) <= (1000 / 1000))
 				continue;
 			lasttime = ev.xmotion.time;
 
