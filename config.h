fbb7e3f0443f22b859b65f4f0e3e31eb31de3454
Silvhr <silvhr@protonmail.com>
2022-07-21 23:33:16 -0500

    changed dmenu line length

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "JetBrains Mono:size=12","JoyPixels:pixelsize=12:antialias=true:autohint"};
-static const char dmenufont[]       = "JetBrains Mono:size=12";
+static const char *fonts[]          = { "JetBrains:size=14:antialias=true:autohint=true", "JetBrains Mono Nerd Font:size=14:antialias=true:autohint=true"};
+static const char dmenufont[]       = "JetBrains Regular Nerd Font Complete:size=12";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static char normbgcolor[]           = "#222222";
@@ -29,7 +29,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5","6"};
+static const char *tags[] = { "", "", "", "", "",""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-l", "4", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
